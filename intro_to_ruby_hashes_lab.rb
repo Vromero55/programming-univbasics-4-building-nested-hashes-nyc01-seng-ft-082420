736c2c55f3f70c563db4e94f1ab6dd4d17d8fb96
Victor Romero <romeroa.victor@yahoo.com>
2020-08-19 14:41:51 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -41,7 +41,7 @@ hash = {:railroads =>{:pieces => 4 ,
 :four_pieces_owned => 200
 },  :names => {:reading_railroad => {:mortgage_value => 100}, 
   :pennsylvania_railroad => {:mortgage_value => 200} ,
-  :b_and_o_railroad => {} ,
-  :shortline_railroad => {}
+  :b_and_o_railroad => {:mortgage_value => 400} ,
+  :shortline_railroad => {:mortgage_value => 800}
 }}}
 end
